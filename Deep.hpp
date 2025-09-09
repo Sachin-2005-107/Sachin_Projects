dd5fdd665b17e923b754e3a8d13c98df6c74e285
Sachin-2005-107 <gksachinerd@gmail.com>
2025-09-09 11:42:43 +0530

    Update Deep.hpp

--- Deep.hpp
+++ Deep.hpp
@@ -49,9 +49,54 @@ class layer{
 };
 class neural{
     vector<layer> nn;
-    Matrix data;
-    public:
-    neural(string s){
-        data(s);
+    Matrix data,out;
+    int n; //number of layers in the neural network
+    float LCE(int i){
+        float y=out.get_element(i,0);
+        float p=nn[0].a.get_element(0,0);
+        return (-y/p)+((1-y)/(1-p));
     }
+    float MSE(int i){
+        float y=out.get_element(i,0);
+        float p=nn[0].a.get_element(0,0);
+        return p-y;
+    }
+    public:
+    neural(int n , string s){
+        data.initialise(s);
+        this->n = n;
+        out=data.get_col(data.col()-1);
+        data=data.features();
+    }
+    void nn_init(){
+        
+        for(int i=0;i<n;i++){
+            string s;
+            int x;
+            cout<<"Enter the number of neurons in the "<<i<<"th layer and the activation function of each layer assuming the 0th layer is the output layer"<<endl;
+            cin>>x;
+            getline(cin,s);
+            nn.push_back(layer(x,s));
+        }
+        cout<<"The neural network architecture is successfully created"<<endl;
+    }
+    void forward(int x){
+        for(int i=n-1;i>0;i--){ //going from the last layer to the first layer
+            if(i==n-1){
+                nn[i].linear(data.get_row(x).transpose());
+                nn[i].activation();
+            }
+            else{
+                nn[i].linear(nn[i+1].a);
+                nn[i].activation();
+            }
+        }
+        cout<<"The forward propagation was successfully \n";
+    }
+    void backward(){
+        if(nn[0].s=="relu");
+         //does something with mse
+        else if(nn[0].s=="sigmoid");
+         //does something with lce
+    } 
 };
