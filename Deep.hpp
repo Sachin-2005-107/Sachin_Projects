2653349d3de9262b5ad136fe89b43623262b9e73
Sachin-2005-107 <gksachinerd@gmail.com>
2025-09-10 16:56:21 +0530

    Update Deep.hpp

--- Deep.hpp
+++ Deep.hpp
@@ -32,7 +32,8 @@ class layer{
             b.resizer(n,1,0);
             l.resizer(n,1,0);
             a_error.resizer(1,n,0);
-            
+            w.resizer(n,1,0);
+            del.resizer(1,1,0);
         }
         void activation(){
             if(s=="relu") 
@@ -41,14 +42,16 @@ class layer{
             return;
         }
         void linear(Matrix a_prev) {
-        if(w.row() != a_prev.row()) {
-            w.resizer(n, a_prev.row(), 0.1);  // deterministic init
+        if(w.col() != a_prev.row()) {
+            w.resizer(n, a_prev.row(), 0.1); 
+            
         }
         l = ( w*a_prev) + b;
+        this->w.printdim();
     }
 };
 class neural{
-    vector<layer> nn;
+    
     Matrix data,out;
     int n; //number of layers in the neural network
     float LCE(int i){
@@ -62,6 +65,7 @@ class neural{
         return p-y;
     }
     public:
+    vector<layer> nn;
     neural(int n , string s){
         data.initialise(s);
         this->n = n;
@@ -73,7 +77,6 @@ class neural{
         for(int i=0;i<n;i++){
             string s;
             int x;
-            cout<<"Enter the number of neurons in the "<<i<<"th layer and the activation function of each layer assuming the 0th layer is the output layer"<<endl;
             cin>>x;
             getline(cin,s);
             nn.push_back(layer(x,s));
@@ -81,22 +84,65 @@ class neural{
         cout<<"The neural network architecture is successfully created"<<endl;
     }
     void forward(int x){
-        for(int i=n-1;i>0;i--){ //going from the last layer to the first layer
+        for(int i = n-1 ; i>=0 ; i-- ){ //going from the last layer to the first layer
             if(i==n-1){
+                cout<<i<<endl;
+              
                 nn[i].linear(data.get_row(x).transpose());
                 nn[i].activation();
+               // nn[i].l.printdim();
             }
             else{
+                cout<<i<<endl;
+               
                 nn[i].linear(nn[i+1].a);
                 nn[i].activation();
+                //nn[i].l.printdim();
             }
         }
         cout<<"The forward propagation was successfully \n";
     }
-    void backward(){
-        if(nn[0].s=="relu");
-         //does something with mse
-        else if(nn[0].s=="sigmoid");
-         //does something with lce
+    void backward( int j ){
+       float x=0.1;
+       cout<<"OK0"<<endl;
+       
+        float error=LCE(j);
+        nn[0].del=nn[0].a_error;
+        nn[0].del.scalarmul(error);
+        nn[0].del.printdim();
+         cout<<"OK"<<endl;
+     for(int i=1;i<10;i++){
+        cout<<i<<endl;
+        cout<<"OK1"<<endl;
+        nn[i].del.printdim();
+        nn[i-1].w.printdim();
+        nn[i].a_error.printdim();
+        nn[i].del=nn[i-1].w.transpose()*nn[i-1].del;
+        nn[i].del=nn[i].del.habdard(nn[i].a_error);        
+            if(i==n-1){
+                cout<<"OK2"<<endl;
+                Matrix temp=nn[i].del * data.get_row(j);
+                temp.scalarmul(x);
+                nn[i].w = nn[i].w - temp;
+                temp=nn[i].del;
+                temp.scalarmul(x);
+                nn[i].b = nn[i].b - temp;
+            }
+            else{
+                cout<<"OK3"<<endl;
+                nn[i].del.printdim();
+                nn[i-1].a.printdim();
+                Matrix temp=nn[i].del * nn[i-1].a.transpose();
+                cout<<"OK4"<<endl;
+                temp.scalarmul(x);
+                temp.printdim();
+                nn[i].w = nn[i].w - temp;
+                temp=nn[i].del;
+                temp.scalarmul(x);
+                nn[i].b = nn[i].b - temp;
+            }
+        }
+       
+       //cout<<"Backward was successfull"<<endl;
     } 
 };
